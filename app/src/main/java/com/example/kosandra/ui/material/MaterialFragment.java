446fe2aad959241a2757367e951fd1292a57e228
jastpl2153 <jastpl2153@mail.ru>
2023-12-22 19:56:53 +0800

    Проведен рефакторинг кода

--- app/src/main/java/com/example/kosandra/ui/material/MaterialFragment.java
+++ app/src/main/java/com/example/kosandra/ui/material/MaterialFragment.java
@@ -12,12 +12,12 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.kosandra.databinding.FragmentDashboardBinding;
 
-public class DashboardFragment extends Fragment {
+public class MaterialFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
+        MaterialViewModel dashboardViewModel = new ViewModelProvider(this).get(MaterialViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
